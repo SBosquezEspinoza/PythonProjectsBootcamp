adee615a8410209623f5eaf11211c38798705991
Santiago Bosquez <santib831@gmail.com>
2023-09-26 09:39:04 +0200

    Last updated removing some items from the navbar

--- day68-Advanced-Authenticatio-with-Flask/main.py
+++ day68-Advanced-Authenticatio-with-Flask/main.py
@@ -35,7 +35,7 @@ with app.app_context():
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/register', methods=["POST", "GET"])
@@ -56,7 +56,7 @@ def register():
         db.session.commit()
         login_user(new_user)
         return redirect(url_for('secrets'))
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/login', methods=["GET", "POST"])
@@ -72,13 +72,13 @@ def login():
         else:
             flash('The email does not exist')
             return redirect(url_for('login'))
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route('/secrets')
 @login_required
 def secrets():
-    return render_template("secrets.html", user=current_user)
+    return render_template("secrets.html", user=current_user, logged_in=True)
 
 
 @app.route('/logout')
